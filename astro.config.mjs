5fd05a7094045d63b3a5c29c2480c0985064d60f
tenpaMk2 <wavyroom@gmail.com>
2023-10-08 23:11:27 +0900

    :zap: Improve URL control.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tenpamk2-blog.netlify.app/",
+  base: "/apps/sd-character-prompts-builder/",
   integrations: [
     partytown({
       // Adds dataLayer.push as a forwarding-event.
