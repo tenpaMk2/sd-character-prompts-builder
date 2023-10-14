f0d704999df5d2019686d3de84505c876d5fd5ba
tenpaMk2 <wavyroom@gmail.com>
2023-10-14 19:10:31 +0900

    :bug: Fix forgot exporting.

--- src/libs/util.ts
+++ src/libs/util.ts
@@ -1,6 +1,8 @@
 // Workaround for the problem of vanilla `Object.keys()` returning not union but `string[]` type
 // even though object has fixed keys.
 // <https://zenn.dev/ossamoon/articles/694a601ee62526>
-const getKeys = <T extends { [key: string]: unknown }>(obj: T): (keyof T)[] => {
+export const getKeys = <T extends { [key: string]: unknown }>(
+  obj: T,
+): (keyof T)[] => {
   return Object.keys(obj);
 };
