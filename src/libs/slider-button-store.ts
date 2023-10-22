6fb9853bb4bfbf18b36b977034c8159e3648040f
tenpaMk2 <wavyroom@gmail.com>
2023-10-22 17:08:25 +0900

    :recycle: Refactor to use common button.

--- src/libs/slider-button-store.ts
+++
@@ -1,8 +0,0 @@
-import { atom, type WritableAtom } from "nanostores";
-
-export const sliderButtonKeys = [`oneLine`] as const;
-export type SliderButtonKeys = (typeof sliderButtonKeys)[number];
-export const sliderButtonStoreMap: ReadonlyMap<
-  SliderButtonKeys,
-  WritableAtom<boolean>
-> = new Map([[`oneLine`, atom(true)]]);
