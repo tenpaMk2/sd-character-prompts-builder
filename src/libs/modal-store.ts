ddffdaa4ddbd583b60e00f0a68ec9cbb6f7d507a
tenpaMk2 <wavyroom@gmail.com>
2023-10-22 11:28:22 +0900

    :bug: Fix forgotten default color modal defines.

--- src/libs/modal-store.ts
+++ src/libs/modal-store.ts
@@ -4,5 +4,8 @@ import { type ColorCategory } from "./prompts-store";
 const defaultEnable: { readonly [K in ColorCategory]: boolean } = {
   "eyes-color": false,
   "hair-color": false,
+  "bow-color": false,
+  "ribbon-color": false,
+  "hairband-color": false,
 };
 export const modalEnableStore = map(defaultEnable);
